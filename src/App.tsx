87b848da97882b3517924ac4013d887a1810ef1c
Luis <luiscarlossfaria@gmail.com>
2022-06-06 12:41:58 -0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+
 
 function App() {
+  const [userData, setUserData] = useState({login: ""});
+
+  useEffect(() => {
+    const url = "https://api.github.com/users/luiscfaria";
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        setUserData(json);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+        <h1>{userData.login}</h1>
+      </>
     </div>
   );
 }
